4bd242ae2ed8addf0e20cdafe09737e83511701f
Jaba-chan <kuzakovevgen@gmail.com>
2024-05-19 21:50:50 +0700

    Initial commit

---
+++ THP_LR4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Numerics;
+using Weapons;
+using Logic;
+using Logic.Interfaceies;
+using Logic.Creatories;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        PrintSum printSum = new PrintSum();
+        MilitaryUnitCreator militaryUnitCreator = new MilitaryUnitCreator();
+        CalculatePrice calculatePrice = new CalculatePrice();
+        MilitaryUnit unit = militaryUnitCreator.create();
+        
+        printSum.Print(calculatePrice.Calculate(unit));
+    }
+}
